4be9a5999d4fa67c8bfe4e155e7235bdd01dac56
DajeongYun <ydajeong7@gmail.com>
2023-07-13 17:29:18 +0900

    Refactor: Change field name from Address

--- src/main/java/com/widzard/bidking/common/entity/Address.java
+++ src/main/java/com/widzard/bidking/common/entity/Address.java
@@ -7,71 +7,53 @@ import java.util.Objects;
 public class Address {
 
 
-    private String sido; // (시도)
-    private String gugun; // (구군)
-    private String addressDetails; // (상세주소)
-    private Integer zipcode; //우편번호  //TODO String으로 바꿈
+    private String street; // 도로명 주소
+    private String details; // (상세주소)
+    private String zipcode; //우편번호
 
     protected Address() {
     }
 
-    public Address(String sido, String gugun, String addressDetails, Integer zipcode) {
-        this.sido = sido;
-        this.gugun = gugun;
-        this.addressDetails = addressDetails;
+    public Address(String street, String details, String zipcode) {
+        this.street = street;
+        this.details = details;
         this.zipcode = zipcode;
     }
 
-
-    public String getSido() {
-        return sido;
+    public String getStreet() {
+        return street;
     }
 
-    private void setSido(String sido) {
-        this.sido = sido;
+    public String getDetails() {
+        return details;
     }
 
-    public String getGugun() {
-        return gugun;
-    }
-
-    private void setGugun(String gugun) {
-        this.gugun = gugun;
-    }
-
-    public String getAddressDetails() {
-        return addressDetails;
-    }
-
-    private void setAddressDetails(String addressDetails) {
-        this.addressDetails = addressDetails;
-    }
-
-    public Integer getZipcode() {
+    public String getZipcode() {
         return zipcode;
     }
 
-    private void setZipcode(Integer zipcode) {
+    private void setStreet(String street) {
+        this.street = street;
+    }
+
+    private void setDetails(String details) {
+        this.details = details;
+    }
+
+    private void setZipcode(String zipcode) {
         this.zipcode = zipcode;
     }
 
     @Override
     public boolean equals(Object o) {
-        if (this == o) {
-            return true;
-        }
-        if (o == null || getClass() != o.getClass()) {
-            return false;
-        }
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
         Address address = (Address) o;
-        return Objects.equals(getSido(), address.getSido()) && Objects.equals(
-                getGugun(), address.getGugun()) && Objects.equals(getAddressDetails(),
-                address.getAddressDetails()) && Objects.equals(getZipcode(),
-                address.getZipcode());
+        return Objects.equals(getStreet(), address.getStreet()) && Objects.equals(getDetails(), address.getDetails()) && Objects.equals(getZipcode(), address.getZipcode());
     }
 
     @Override
     public int hashCode() {
-        return Objects.hash(getSido(), getGugun(), getAddressDetails(), getZipcode());
+        return Objects.hash(getStreet(), getDetails(), getZipcode());
     }
 }
