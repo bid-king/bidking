a7854797a4f6e944f92c138cb7d3e520f7d6fb1a
DajeongYun <ydajeong7@gmail.com>
2023-07-17 10:39:12 +0900

    Refactor: Separate OrderState

--- src/main/java/com/widzard/bidking/order/entity/OrderState.java
+++ src/main/java/com/widzard/bidking/order/entity/OrderState.java
@@ -1,9 +1,6 @@
 package com.widzard.bidking.order.entity;
 
 public enum OrderState {
-    PAYMENT_WAITING,
-    PAYMENT_OK,
-    PAYMENT_CANCELED,
     ORDER_SUCCESS,
     ORDER_CANCELED;
 
---
+++ src/main/java/com/widzard/bidking/payment/entity/PaymentState.java
@@ -0,0 +1,8 @@
+package com.widzard.bidking.payment.entity;
+
+public enum PaymentState {
+
+    PAYMENT_WAITING,
+    PAYMENT_OK,
+    PAYMENT_CANCELED,
+}
