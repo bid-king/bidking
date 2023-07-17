81b7707fa46da36dce1b1099f91f75c340403a68
yongseong2 <ancjs369@naver.com>
2023-07-17 13:19:30 +0900

    Feat: add theme colors, static properties

---
+++ client/src/_libs/design/colors.ts
@@ -0,0 +1,31 @@
+const white = '#FFFFFF';
+const confirm = '#FBB708';
+const warn = '#DD3500';
+const progress = '#441CE4';
+const ok = '#20E9B9';
+const black = '#333333';
+
+export default {
+  //Theme Colors
+  white,
+  confirm,
+  warn,
+  progress,
+  ok,
+  black,
+  //unactivated colors
+  confirm33: 'rgba(251, 183, 8, 0.5)',
+  warn33: 'rgba(251,183,8, 0.5)',
+  progress33: 'rgba(80, 28, 224, 0.5)',
+  ok33: 'rgba(32,233,185, 0.5)',
+  //greys
+  grey: '#666666',
+  lightgrey: '#AAAAAA',
+  whitegrey: '#EEEEEE',
+  backgroundLight: '#EFEFF3',
+  backgroundLight2: '#FFFFFF',
+  backgroundLight3: '#DEDEE4',
+  backgroundDark: '#16161D',
+  backgroundDark2: '#33333C',
+  backgroundDark3: '#55555F',
+};
