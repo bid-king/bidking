dd163f25722cdcf5a42bb8bb515908171baca8b3
brewcoldblue <seung.yoon.yu@gmail.com>
2023-07-17 15:36:39 +0900

    feat/add TextArea component

---
+++ client/src/_libs/components/TextArea.stories.ts
@@ -0,0 +1,19 @@
+import type { Meta, StoryObj } from '@storybook/react';
+
+import { TextArea } from './TextArea';
+
+const meta = {
+  title: 'Components/TextArea',
+  component: TextArea,
+  argTypes: {},
+} satisfies Meta<typeof TextArea>;
+
+export default meta;
+type Story = StoryObj<typeof meta>;
+
+export const Primary: Story = {
+  args: {
+    theme: 'light',
+    placeholder: '안녕 나는 텍스트 에어리어야',
+  },
+};
---
+++ client/src/_libs/components/TextArea.tsx
@@ -0,0 +1,44 @@
+/** @jsxImportSource @emotion/react */
+import React from 'react';
+import colors from '../design/colors';
+
+interface Props {
+  theme?: 'light' | 'dark';
+  placeholder: string;
+}
+
+export function TextArea({
+  placeholder = 'placeholder를 입력하세요',
+  theme = 'light',
+}: Props) {
+  return (
+    <textarea
+      placeholder={placeholder}
+      css={{
+        width: '100%',
+        height: '10rem',
+        padding: '1rem',
+        fontWeight: 400,
+        outline: 'none',
+        borderRadius: '1rem',
+        resize: 'none',
+        ...THEME_VARIENT[theme],
+      }}
+    />
+  );
+}
+
+const THEME_VARIENT = {
+  light: {
+    backgroundColor: colors.backgroundLight2,
+    border: `1px solid ${colors.grey}`,
+    color: colors.black,
+    '::placeholder': { color: colors.grey, fontWeight: 700 },
+  },
+  dark: {
+    backgroundColor: colors.backgroundDark3,
+    border: `1px solid ${colors.backgroundDark3}`,
+    color: colors.white,
+    '::placeholder': { color: colors.lightgrey, fontWeight: 700 },
+  },
+};
