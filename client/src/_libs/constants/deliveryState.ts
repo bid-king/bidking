9c86a3bfd22f817669bc3a44fca476b2c9161809
brewcoldblue <seung.yoon.yu@gmail.com>
2023-07-18 14:05:32 +0900

    Feat: add state constants

---
+++ client/src/_libs/constants/deliveryState.ts
@@ -0,0 +1,5 @@
+export default {
+  DELIVERY_WAITING: '아직 배송이 시작되지 않았어요',
+  DELIVERING: '상품이 배송 중이에요',
+  DELIVERY_COMPLETED: '수령을 완료했어요',
+};
---
+++ client/src/_libs/constants/orderState.ts
@@ -0,0 +1,4 @@
+export default {
+  ORDER_SUCCESS: '낙찰된 상품이에요',
+  ORDER_CANCELED: '낙찰되었지만 결제하지 않았어요',
+};
