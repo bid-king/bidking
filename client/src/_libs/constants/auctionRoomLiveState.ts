658f4c7d196f5f7f3252fa717cfb5386688137a8
brewcoldblue <seung.yoon.yu@gmail.com>
2023-07-18 16:07:20 +0900

    Fix: modify order states

--- client/src/_libs/constants/auctionRoomLiveState.ts
+++ client/src/_libs/constants/auctionRoomLiveState.ts
@@ -3,3 +3,4 @@ export default {
   ON_LIVE: '경매가 진행 중이에요',
   OFF_LIVE: '종료된 경매에요',
 };
+//경매 상세보기
--- client/src/_libs/constants/orderState.ts
+++ client/src/_libs/constants/orderState.ts
@@ -2,3 +2,4 @@ export default {
   ORDER_SUCCESS: '낙찰된 상품이에요',
   ORDER_CANCELED: '낙찰되었지만 결제하지 않았어요',
 };
+//아이템 상세보기
