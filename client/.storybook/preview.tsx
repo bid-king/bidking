bab3dffb1c630aa1bf92f1b1c0e3f249aaf64ada
yongseong2 <ancjs369@naver.com>
2023-07-23 11:12:08 +0900

    Feat: Add NavBar Storybook and fix storybook preview for router

--- client/.storybook/preview.tsx
+++ client/.storybook/preview.tsx
@@ -3,22 +3,25 @@ import React from 'react';
 import { Global, css } from '@emotion/react';
 import type { Preview } from '@storybook/react';
 import { globalStyle } from '../src/_libs/design/globalStyle';
+import { BrowserRouter } from 'react-router-dom';
 
 const preview: Preview = {
   decorators: [
     (Story) => (
       <>
-        <Global styles={globalStyle} />
-        <div
-          css={{
-            position: 'absolute',
-            width: '430px',
-            left: '50%',
-            transform: 'translateX(-50%)',
-          }}
-        >
-          <Story />
-        </div>
+        <BrowserRouter>
+          <Global styles={globalStyle} />
+          <div
+            css={{
+              position: 'absolute',
+              width: '430px',
+              left: '50%',
+              transform: 'translateX(-50%)',
+            }}
+          >
+            <Story />
+          </div>
+        </BrowserRouter>
       </>
     ),
   ],
