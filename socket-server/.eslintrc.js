8363dd32b9a668da1b465594a5f6bd4617e700e6
brewcoldblue <seung.yoon.yu@gmail.com>
2023-08-01 14:12:07 +0900

    Chore: Fix Eslintrc

--- socket-server/.eslintrc.js
+++ socket-server/.eslintrc.js
@@ -1,25 +1,7 @@
 module.exports = {
-  env: { browser: true, es2021: true, node: true },
-  extends: [
-    'eslint:recommended',
-    'plugin:react/recommended',
-    'plugin:storybook/recommended',
-    'plugin:@typescript-eslint/recommended',
-    'prettier',
-  ],
-  overrides: [
-    {
-      // or whatever matches stories specified in .storybook/main.js
-      files: ['*.stories.@(ts|tsx|js|jsx|mjs|cjs)'],
-      rules: {
-        // example of overriding a rule
-        'storybook/hierarchy-separator': 'error',
-        // example of disabling a rule
-        'storybook/default-exports': 'off',
-      },
-    },
-  ],
-  parser: '@typescript-eslint/parser',
+  env: { es2021: true, node: true },
+  extends: ['eslint:recommended', 'prettier'],
+
   parserOptions: {
     ecmaVersion: 'latest',
     sourceType: 'module',
@@ -30,8 +12,6 @@ module.exports = {
     // 'linebreak-style': ['error', 'unix'],
     quotes: ['error', 'single'],
     semi: ['error', 'always'],
-    'react/no-unknown-property': ['error', { ignore: ['css'] }],
-    'no-unused-vars': 'off',
-    '@typescript-eslint/no-unused-vars': 'off',
+    'no-unused-vars': 'warn',
   },
 };
