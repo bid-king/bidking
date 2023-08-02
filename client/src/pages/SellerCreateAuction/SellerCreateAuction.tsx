2fbe6176b273490a44c3b4193960a347a1e783c3
yongseong2 <ancjs369@naver.com>
2023-08-02 17:42:59 +0900

    Feat: Add Auction Room Create Api and MyPage Api connect

--- client/src/pages/SellerCreateAuction/SellerCreateAuction.tsx
+++ client/src/pages/SellerCreateAuction/SellerCreateAuction.tsx
@@ -1,11 +1,19 @@
+/** @jsxImportSource @emotion/react */
 import React from 'react';
-import { AuctionCreate } from './AuctionCreateBox';
+import { AuctionCreateBox } from './AuctionCreateBox';
+import colors from '../../_libs/design/colors';
 
 export function SellerCreateAuction() {
   return (
-    <div>
-      경매등록화면입니다.
-      <AuctionCreate />
+    <div
+      css={{
+        backgroundColor: colors.backgroundDark,
+        display: 'flex',
+        justifyContent: 'center',
+        padding: '2rem',
+      }}
+    >
+      <AuctionCreateBox />
     </div>
   );
 }
