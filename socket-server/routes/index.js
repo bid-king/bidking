9ca4330c7a79f339bc7582b15e3fd6f9aed6051a
정예지 <jeongyeji.mail@gmail.com>
2023-08-04 11:07:30 +0900

    Feat: Add success bid

--- socket-server/routes/index.js
+++ socket-server/routes/index.js
@@ -10,4 +10,12 @@ router.get('/chat', async (req, res, next) => {
   res.render('chat');
 });
 
+router.post('/success', async (req, res, next) => {
+  const io = req.app.get('io');
+  const { roomId, bidInfo } = req.body;
+  // const { item, user, price } = bidInfo;
+  io.to(`${roomId}`).emit('successBid', bidInfo);
+  res.send('ok');
+});
+
 module.exports = router;
