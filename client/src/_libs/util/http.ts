9c4672c8099311dfb6b067ebf9263687376a9c70
yongseong2 <ancjs369@naver.com>
2023-08-06 10:36:57 +0900

    Feat: Add Main Page

--- client/src/_libs/util/http.ts
+++ client/src/_libs/util/http.ts
@@ -1,6 +1,7 @@
 import Axios from 'axios';
 
-export const API_URL = 'http://70.12.247.182:5000';
+// export const API_URL = 'http://70.12.247.182:5000';
+export const API_URL = 'http://localhost:5000';
 
 export async function getToken() {
   const persistedState = sessionStorage.getItem('persist:root');
