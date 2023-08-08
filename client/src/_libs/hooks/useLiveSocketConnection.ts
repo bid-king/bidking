597dc6ef6637723c788b9f5978b1004a3c460bfd
brewcoldblue <seung.yoon.yu@gmail.com>
2023-08-08 12:51:22 +0900

    Feat: Modify Auth

--- client/src/_libs/hooks/useLiveSocketConnection.ts
+++ client/src/_libs/hooks/useLiveSocketConnection.ts
@@ -6,22 +6,21 @@ import { store } from '../../store/store';
 export function useLiveSocketConnection(roomId: number) {
   const socket = useRef(live);
   const [socketConnectionErr, setSocketConnectionErr] = useState<unknown>(null);
+  const [userNickname, setUserNickname] = useState<string>('');
   useEffect(() => {
-    let isLogined: boolean | undefined = false;
-    const userNickname: string | undefined = '정예지';
+    auth();
 
-    (async () => {
+    async function auth() {
       try {
-        isLogined = await store.getState().user.isLogined;
-        // userNickname = await store.getState().user.nickname;
-        if (!isLogined) throw new Error('403');
+        setUserNickname(store.getState().user.nickname);
+        if (!userNickname) throw new Error('403');
         else {
           socket.current.req.connect(roomId, userNickname);
         }
       } catch (err) {
         setSocketConnectionErr(err);
       }
-    })();
+    }
 
     return () => {
       live.req.leave(roomId, userNickname);
