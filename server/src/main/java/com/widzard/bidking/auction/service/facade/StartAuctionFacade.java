0d060a636f7e93f8f7bdcf22f545f007f8a2221e
윤다정 <ydajeong7@gmail.com>
2023-08-11 15:17:44 +0900

    Feat: Refactor configuration

---
+++ server/src/main/java/com/widzard/bidking/auction/service/facade/StartAuctionFacade.java
@@ -0,0 +1,14 @@
+package com.widzard.bidking.auction.service.facade;
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Component;
+
+@Slf4j
+@Component
+@RequiredArgsConstructor
+public class StartAuctionFacade {
+
+
+
+}
