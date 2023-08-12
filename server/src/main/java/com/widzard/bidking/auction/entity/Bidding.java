e9e87b69ea5fe23d4d988c0cf5688004ae8d7e04
정예지 <jeongyeji.mail@gmail.com>
2023-08-12 14:29:44 +0900

    Feat: Change bidding.count nullable

--- server/src/main/java/com/widzard/bidking/auction/entity/Bidding.java
+++ server/src/main/java/com/widzard/bidking/auction/entity/Bidding.java
@@ -2,17 +2,11 @@ package com.widzard.bidking.auction.entity;
 
 
 import com.widzard.bidking.global.entity.BaseEntity;
-import com.widzard.bidking.item.entity.Item;
-import com.widzard.bidking.member.entity.Member;
 import javax.persistence.Column;
 import javax.persistence.Entity;
-import javax.persistence.FetchType;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
-import javax.persistence.JoinColumn;
-import javax.persistence.ManyToOne;
-import javax.persistence.OneToOne;
 import javax.persistence.Table;
 import lombok.AccessLevel;
 import lombok.AllArgsConstructor;
@@ -31,21 +25,22 @@ public class Bidding extends BaseEntity {
     @Column(name = "bidding_id")
     private Long id;
 
-//    @ManyToOne(fetch = FetchType.LAZY)
+    //    @ManyToOne(fetch = FetchType.LAZY)
 //    @JoinColumn(name = "member_id", nullable = false)
     private Long memberId;
 
-//    @OneToOne(fetch = FetchType.LAZY)
+    //    @OneToOne(fetch = FetchType.LAZY)
 //    @JoinColumn(name = "item_id", nullable = false)
     private Long itemId;
 
-//    @Column(nullable = false)
+    //    @Column(nullable = false)
     private Long price;
 
+    @Column(nullable = true)
     private int count;
 
     public void raisePrice(Long price) {
-        if (this.price <= price ) {
+        if (this.price <= price) {
             throw new RuntimeException("현재 입찰가보다 더 높은 가격을 제시해야 합니다.");
         }
         this.price = price;
