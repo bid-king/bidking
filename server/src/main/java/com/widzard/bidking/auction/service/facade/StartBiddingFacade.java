1dd201e0f7944282d5f659653c7534b8aab79d6c
윤다정 <ydajeong7@gmail.com>
2023-08-13 10:19:38 +0900

    Feat: Update CurrentPrice Redis

--- server/src/main/java/com/widzard/bidking/auction/service/facade/StartBiddingFacade.java
+++ server/src/main/java/com/widzard/bidking/auction/service/facade/StartBiddingFacade.java
@@ -7,7 +7,6 @@ import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.stereotype.Component;
-import org.springframework.transaction.annotation.Transactional;
 
 @Slf4j
 @Component
@@ -17,15 +16,21 @@ public class StartBiddingFacade {
     private final RedisTemplate<String, Object> redisTemplate;
     private final AuctionService auctionService;
 
-    @Transactional
+    // TODO Transactional 처리?
     public void startBidding(Member member, Long auctionId, Long itemId) {
         // 경매 시작 처리 (member 검증 및 아이템, 경매방 상태 변경)
         Long startPrice = auctionService.startBidding(member, auctionId, itemId);
 
         // redis 저장
         redisTemplate.opsForValue().set(
-            generateKey(auctionId),
+            generateItemKey(auctionId),
             itemId,
+            Duration.ofDays(1)
+        );
+
+        redisTemplate.opsForValue().set(
+            generateCurrentPriceKey(auctionId),
+            startPrice,
             Duration.ofDays(1)
         );
 
@@ -36,15 +41,22 @@ public class StartBiddingFacade {
         );
     }
 
+
     public void test(Member member, Long auctionId, Long itemId) {
         // redis 저장
         redisTemplate.opsForValue().set(
-            generateKey(auctionId),
+            generateItemKey(auctionId),
             itemId,
             Duration.ofDays(1)
         );
 
-        log.info("generate key: {}", generateKey(auctionId));
+        redisTemplate.opsForValue().set(
+            generateCurrentPriceKey(auctionId),
+            120000L,
+            Duration.ofDays(1)
+        );
+
+        log.info("generate key: {}", generateItemKey(auctionId));
         // 갱신된 입찰가를 Redis 채널에 발행(Publish)
         redisTemplate.convertAndSend(
             "StartAuctionItem",
@@ -52,7 +64,11 @@ public class StartBiddingFacade {
         );
     }
 
-    private String generateKey(Long auctionId) {
+    private String generateCurrentPriceKey(Long auctionId) {
+        return "auction" + ":" + auctionId + ":" + "onLiveItem:currentPrice";
+    }
+
+    private String generateItemKey(Long auctionId) {
         return "auction" + ":" + auctionId + ":" + "onLiveItem:itemId";
     }
 
