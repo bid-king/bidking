75e71f95a38a38ce275d1eee0e732f3aae7b035f
brewcoldblue <seung.yoon.yu@gmail.com>
2023-08-14 10:04:02 +0900

    Fix: Fix Effect Hook in Live Enter

--- client/src/_libs/hooks/useLiveEnter.ts
+++ client/src/_libs/hooks/useLiveEnter.ts
@@ -19,6 +19,8 @@ export function useLiveEnter() {
   const [socketConnectionErr, setSocketConnectionErr] = useState<unknown>(null);
 
   useEffect(() => {
+    getRoomInfo();
+
     async function getRoomInfo() {
       const uid = (await store.getState().user.id) || 0;
       enter(Number(auctionId), accessToken)
@@ -29,22 +31,17 @@ export function useLiveEnter() {
           setSeller(data.seller);
           setTitle(data.title);
           setAuctionRoomType(data.auctionRoomType);
+          return data;
         })
         .then(res => {
           socket.current = io('http://localhost:8005', {
             withCredentials: true,
           });
-          live(socket.current).send.connect(auctionRoomId, nickname, seller);
+          live(socket.current).send.connect(res.auctionRoomId, res.nickname, res.seller);
         })
         .catch(err => setLiveAuthErr(err));
     }
-    try {
-      (async () => {
-        await getRoomInfo();
-      })();
-    } catch (err) {
-      setLiveAuthErr(err);
-    }
+
     return () => {
       live(socket.current).send.leave(auctionRoomId, nickname);
     }; //unmount시 채팅방 나갑니다
