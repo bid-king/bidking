0f68a5601c317b4ba294d0f1fe919709f5aa0cb1
김동현 <dhkim960511@naver.com>
2023-08-15 13:18:42 +0900

    Feat: Add created time at alarm response

--- server/src/main/java/com/widzard/bidking/alarm/dto/response/AlarmResponse.java
+++ server/src/main/java/com/widzard/bidking/alarm/dto/response/AlarmResponse.java
@@ -3,6 +3,7 @@ package com.widzard.bidking.alarm.dto.response;
 import com.widzard.bidking.alarm.entity.Alarm;
 import com.widzard.bidking.alarm.entity.AlarmType;
 import com.widzard.bidking.alarm.entity.Content;
+import java.time.LocalDateTime;
 import lombok.AccessLevel;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
@@ -21,12 +22,15 @@ public class AlarmResponse {
 
     private String content;
 
+    private LocalDateTime createdTime;
+
     private AlarmType alarmType;
 
     public static AlarmResponse from(Alarm alarm) {
         return AlarmResponse.builder()
             .id(alarm.getId())
             .content(alarm.getContent().getContent())
+            .createdTime(alarm.getCreatedAt())
             .alarmType(alarm.getAlarmType())
             .build();
     }
