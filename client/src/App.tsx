1d2c2b28495da69cdc678870a211eb7636b45b0b
yongseong2 <ancjs369@naver.com>
2023-08-15 16:04:17 +0900

    Feat: Add Enter Auction Room

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,14 +1,12 @@
 import { Global, css } from '@emotion/react';
 import React from 'react';
 import { AppRouter } from './router/AppRouter';
-import { LinkDebug } from './LinkDebug';
 import { globalStyle } from './_libs/design/globalStyle';
 
 function App() {
   return (
     <>
       <Global styles={globalStyle} />
-      <LinkDebug />
       <AppRouter />
     </>
   );
