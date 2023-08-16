5dd131dd6b46fb3073ffc5f947cca23fac16f54e
정예지 <jeongyeji.mail@gmail.com>
2023-08-16 17:31:30 +0900

    Refactor: Remove unused dependencies

--- socket-server/app.js
+++ socket-server/app.js
@@ -1,41 +1,21 @@
 const express = require('express');
 const path = require('path');
 const morgan = require('morgan');
-const cookieParser = require('cookie-parser');
-const session = require('express-session');
 const dotenv = require('dotenv');
 const helmet = require('helmet');
 const hpp = require('hpp');
-const ejs = require('ejs');
 const redisSub = require('./config/pubsub');
-const https = require("https");
-const fs = require("fs");
+const https = require('https');
+const fs = require('fs');
 
 dotenv.config();
 const redis = require('./config/redis');
 const webSocket = require('./socket');
-const indexRouter = require('./routes');
 
 const app = express();
 app.set('port', process.env.PORT || 8005);
 app.set('views', __dirname + '/views');
-app.engine('html', ejs.renderFile);
 app.set('view engine', 'html');
-
-const sessionOption = {
-  resave: false,
-  saveUninitialized: false,
-  secret: process.env.COOKIE_SECRET,
-  cookie: {
-    httpOnly: true,
-    secure: false,
-  },
-};
-if (process.env.NODE_ENV === 'production') {
-  sessionOption.proxy = true;
-  sessionOption.cookie.secure = true;
-}
-const sessionMiddleware = session(sessionOption);
 
 if (process.env.NODE_ENV === 'production') {
   app.use(morgan('combined'));
@@ -47,12 +27,8 @@ if (process.env.NODE_ENV === 'production') {
 app.use(express.static(path.join(__dirname, 'public')));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
-app.use(cookieParser(process.env.COOKIE_SECRET));
-app.use(sessionMiddleware);
 
 redis(app);
-
-app.use('/live/v1', indexRouter);
 
 app.use((req, res, next) => {
   const error = new Error(`${req.method} ${req.url} 라우터가 없습니다.`);
@@ -64,7 +40,6 @@ app.use((err, req, res, next) => {
   res.locals.message = err.message;
   res.locals.error = process.env.NODE_ENV !== 'production' ? err : {};
   res.status(err.status || 500);
-  res.render('error');
 });
 
 // SSL 인증서와 개인 키 파일의 경로
@@ -74,7 +49,7 @@ const certificatePath = './ssl/fullchain.pem';
 // SSL 옵션 설정
 const sslOptions = {
   key: fs.readFileSync(privateKeyPath),
-  cert: fs.readFileSync(certificatePath)
+  cert: fs.readFileSync(certificatePath),
 };
 
 // Express 애플리케이션을 HTTPS로 실행
@@ -84,5 +59,5 @@ server.listen(app.get('port'), () => {
   console.log(app.get('port'), '번 포트에서 대기 중');
 });
 
-webSocket(server, app, sessionMiddleware);
+webSocket(server, app);
 redisSub(app);
