51d9cecb66cd9de0450654c38720462bcf263459
brewcoldblue <seung.yoon.yu@gmail.com>
2023-08-17 14:09:13 +0900

    Fix: Delete Alert Test

--- client/src/pages/Main/Main.tsx
+++ client/src/pages/Main/Main.tsx
@@ -1,12 +1,10 @@
 /** @jsxImportSource @emotion/react */
 import React from 'react';
-import { SellerStream } from '../../_libs/components/meeting/SellerStream';
 import colors from '../../_libs/design/colors';
 import { useAlert } from '../../_libs/hooks/useAlert';
 import { MainBox } from './MainBox';
 
 export function Main() {
-  const { Alert, alertTrigger } = useAlert('ㅋㅋㅋ', 'error');
   return (
     <div
       css={{
@@ -14,8 +12,6 @@ export function Main() {
         minHeight: '100vh',
       }}
     >
-      <button onClick={alertTrigger}>Trigger Success Alert</button>
-      <div>{Alert}</div>
       <MainBox />
     </div>
   );
