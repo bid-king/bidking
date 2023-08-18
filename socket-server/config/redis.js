8503d873e9e8d87cc144588ab4bdadb135761e54
정예지 <jyj2487@naver.com>
2023-08-18 17:48:49 +0900

    Feat: Hide redis password

--- socket-server/config/redis.js
+++ socket-server/config/redis.js
@@ -3,7 +3,7 @@ const redis = require('redis');
 module.exports = app => {
   const client = redis.createClient({
     url: process.env.REDIS_URL,
-    password: "0613"
+    password: process.env.REDIS_PASSWORD,
   });
   app.set('redisCli', client);
 };
